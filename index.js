94b7b46c606199db06379e067ab8dfa989444123
thanhdv <65655156+thanhdv2287@users.noreply.github.com>
2023-04-21 10:34:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From dvthanh2" }');
+    res.send('{ "response": "Hello From dvthanh2_38" }');
 });
 
 app.get('/will', function (req, res) {
